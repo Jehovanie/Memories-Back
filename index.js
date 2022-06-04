20a7ab2efd84f90c5fc92b150098f3c98bbb66d4
Jehovanie <jehovanieram@gmail>
2022-06-04 08:06:33 +0200

    setting up the connection to the mongo Atlas

--- index.js
+++ index.js
@@ -41,12 +41,19 @@ app.use(cors());
 
 /** ==============  CONNECTION TO THE MONGODB =============== */
 
-const CONNECTION_URL = "mongodb://localhost:27017/mernStack"; /// uncomment this to use dotenv
+// const CONNECTION_URL = "mongodb://localhost:27017/mernStack"; /// uncomment this to use dotenv
+const CONNECTION_URL = "mongodb+srv://jehovanieram:jehovanieram1998@cluster0.g2ner.mongodb.net/?retryWrites=true&w=majority"; /// uncomment this to use dotenv
+
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL) /// change connection_url to process.env.CONNECTION_URL
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`)))
     .catch((error) => console.log("ERROR : " + error.message));
+
+
+// mongoose.connect(CONNECTION_URL) /// change connection_url to process.env.CONNECTION_URL
+//     .then(() => app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`)))
+//     .catch((error) => console.log("ERROR : " + error.message));
 
 /** ======================== ROUTES DEFINITIONS ========================== */
 
